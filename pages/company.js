cb7518a2e9ef13c4083f808e5e44111730b50981
JKCodeIN <jamshedshokh@mail.ru>
2022-04-19 12:57:24 -0400

    add to pages

---
+++ pages/company.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function About() {
+  return (
+    <div className="container">
+      Company
+    </div>
+  )
+}
